73a2178cb69e9e363a7d5e00c7b787865beb758e
antoine-mrn <128912051+antoine-mrn@users.noreply.github.com>
2023-05-20 15:32:41 +0200

    Compare card function

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const imagesArray = ["apple", "banana", "brocoli", "cherry", "pepper", "straw", "apple", "banana", "brocoli", "cherry", "pepper", "straw"]
 
 imagesShuffle = fisherYatesShuffle(imagesArray)
+console.log(imagesShuffle)
 
 function fisherYatesShuffle(arr){
     for(let i =arr.length-1 ; i>0 ;i--){
@@ -11,7 +12,6 @@ function fisherYatesShuffle(arr){
 }
 
 const allCards = document.querySelectorAll(".card")
-console.dir(allCards)
 
 function initGame() {
     for(let i = 0; i < imagesShuffle.length; i++) {
@@ -23,14 +23,39 @@ function initGame() {
 allCards.forEach(card => card.addEventListener("click", pickCards))
 
 let cardPicked = []
+let lockReturn = false
 
 function pickCards(e) {
-    e.target.classList.add("rotate")
-    cardPicked.push({target: e.target, image: e.target.children[1].children[0].alt})
+    if(lockReturn) return
 
-    if(cardPicked.length === 2) {
-        compareCards()
+    cardPicked.push({target: e.target, image: e.target.children[1].children[0].alt})
+    e.target.classList.add("rotate")
+
+    if(cardPicked.length === 2 && !lockReturn) {
+        lockReturn = true
+        compareCards(cardPicked)
     }
+
+}
+
+let nbRound = 0
+let result = 0
+
+function compareCards(cards) {
+    nbRound++
+
+    if(cards[0].image === cards[1].image) {
+        cards.forEach(card => card.target.removeEventListener("click", pickCards))
+        result++
+    }
+    else {
+        setTimeout(() => {
+            cards.forEach(card => card.target.classList.remove("rotate"))
+            lockReturn = false
+        }, 1000);
+
+    }
+    cardPicked = []
 }
 
 initGame()
